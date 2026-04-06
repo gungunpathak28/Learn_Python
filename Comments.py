fe66b814ab2791055df5eb131d420e65a66c7888
Gungun pathak <gungunpathak.2006@gmail.com>
2026-04-06 21:26:56 +0530

    Add files via upload

---
+++ Comments.py
@@ -0,0 +1,12 @@
+#This is a Multiline comments
+#written in
+#more than just one line
+print("Hello,!")
+
+
+"""
+This is a Multi line string 
+written in
+more than just one line
+"""
+print("Hello, AALO")
